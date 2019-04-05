903c6e9238351ee7e11995ff4919cee16e9795a7
jrbcruz <joshua_cruz1998@yahoo.com.ph>
2019-04-05 23:54:00 +0800

    Add files via upload

--- lex.yy.c
+++ lex.yy.c
@@ -286,27 +286,27 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 27
-#define YY_END_OF_BUFFER 28
-static yyconst short int yy_acclist[74] =
+#define YY_NUM_RULES 30
+#define YY_END_OF_BUFFER 31
+static yyconst short int yy_acclist[78] =
     {   0,
-       28,   26,   27,    1,   26,   27,    1,   27,   21,   26,
-       27,    9,   26,   27,   20,   26,   27,   11,   26,   27,
-       12,   26,   27,    7,   26,   27,    5,   26,   27,   17,
-       26,   27,    6,   26,   27,   19,   26,   27,    8,   26,
-       27,   22,   26,   27,   16,   26,   27,   18,   26,   27,
-       14,   26,   27,   13,   26,   27,   15,   26,   27,   10,
-       22,    2,   23,   24,    2,    4,    3,    4,   23,   25,
-        4,    3,    4
+       31,   29,   30,    1,   29,   30,    1,   30,   23,   29,
+       30,   11,   29,   30,   22,   29,   30,   13,   29,   30,
+       14,   29,   30,    9,   29,   30,    7,   29,   30,   19,
+       29,   30,    8,   29,   30,   21,   29,   30,   10,   29,
+       30,   24,   29,   30,   18,   29,   30,   20,   29,   30,
+       16,   29,   30,   15,   29,   30,   17,   29,   30,   12,
+       24,    2,   25,   26,    2,    4,    3,    4,   25,   28,
+        4,    3,    4,    5,   27,    5,    6
     } ;
 
-static yyconst short int yy_accept[42] =
+static yyconst short int yy_accept[47] =
     {   0,
         1,    1,    1,    2,    4,    7,    9,   12,   15,   18,
        21,   24,   27,   30,   33,   36,   39,   42,   45,   48,
        51,   54,   57,   60,   61,   61,   62,   62,   62,   63,
-       64,   64,   65,   66,   67,   69,   70,   71,   72,   74,
-       74
+       64,   64,   65,   66,   67,   69,   70,   70,   71,   72,
+       74,   75,   76,   77,   78,   78
     } ;
 
 static yyconst int yy_ec[256] =
@@ -343,51 +343,59 @@ static yyconst int yy_ec[256] =
 
 static yyconst int yy_meta[23] =
     {   0,
-        1,    1,    2,    1,    1,    1,    1,    1,    1,    1,
+        1,    2,    3,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        3,    3
+        4,    1
     } ;
 
-static yyconst short int yy_base[46] =
+static yyconst short int yy_base[54] =
     {   0,
-        0,    0,   49,   50,   50,   50,   50,   50,   50,   50,
-       50,   39,   50,   50,   50,   50,   50,   10,   50,   50,
-       50,   50,   50,   50,   32,    0,   14,   30,    0,   14,
-       27,   22,    0,    0,    0,   15,   13,    0,    0,   50,
-       32,   35,   38,   40,   43
+        0,    0,   68,   69,   69,   69,   69,   69,   69,   69,
+       69,   57,   69,   69,   69,   69,   69,   10,   69,   69,
+       69,   69,   69,   69,   50,    0,   14,   47,    0,   14,
+       39,   35,    0,    0,    0,   13,   28,   19,    0,    0,
+        0,   18,    0,   15,   69,   36,   40,   44,   47,   51,
+       55,   59,   63
     } ;
 
-static yyconst short int yy_def[46] =
+static yyconst short int yy_def[54] =
     {   0,
-       40,    1,   40,   40,   40,   40,   40,   40,   40,   40,
-       40,   40,   40,   40,   40,   40,   40,   40,   40,   40,
-       40,   40,   40,   40,   41,   18,   40,   40,   42,   43,
-       40,   40,   42,   44,   45,   40,   40,   44,   45,    0,
-       40,   40,   40,   40,   40
+       45,    1,   45,   45,   45,   45,   45,   45,   45,   45,
+       45,   45,   45,   45,   45,   45,   45,   45,   45,   45,
+       45,   45,   45,   45,   46,   18,   45,   45,   47,   48,
+       45,   45,   47,   49,   50,   45,   51,   45,   49,   50,
+       52,   53,   52,   53,    0,   45,   45,   45,   45,   45,
+       45,   45,   45
     } ;
 
-static yyconst short int yy_nxt[73] =
+static yyconst short int yy_nxt[92] =
     {   0,
         4,    5,    6,    7,    8,    9,   10,   11,   12,   13,
        14,   15,   16,   17,   18,   19,   20,   21,   22,   23,
-        4,    4,   25,   31,   26,   31,   35,   37,   36,   36,
-       27,   28,   29,   29,   29,   33,   32,   33,   34,   34,
-       38,   37,   38,   39,   32,   39,   30,   24,   40,    3,
-       40,   40,   40,   40,   40,   40,   40,   40,   40,   40,
-       40,   40,   40,   40,   40,   40,   40,   40,   40,   40,
-       40,   40
+        4,    4,   25,   31,   26,   31,   35,   36,   36,   45,
+       27,   28,   42,   38,   37,   37,   29,   29,   29,   29,
+       33,   33,   42,   33,   34,   34,   34,   39,   39,   32,
+       39,   40,   40,   38,   40,   41,   41,   41,   41,   43,
+       43,   32,   43,   44,   30,   24,   44,   45,    3,   45,
+       45,   45,   45,   45,   45,   45,   45,   45,   45,   45,
+       45,   45,   45,   45,   45,   45,   45,   45,   45,   45,
+       45
+
     } ;
 
-static yyconst short int yy_chk[73] =
+static yyconst short int yy_chk[92] =
     {   0,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,   18,   27,   18,   27,   30,   37,   30,   36,
-       18,   18,   41,   41,   41,   42,   32,   42,   43,   43,
-       44,   31,   44,   45,   28,   45,   25,   12,    3,   40,
-       40,   40,   40,   40,   40,   40,   40,   40,   40,   40,
-       40,   40,   40,   40,   40,   40,   40,   40,   40,   40,
-       40,   40
+        1,    1,   18,   27,   18,   27,   30,   36,   30,   44,
+       18,   18,   42,   38,   36,   30,   46,   46,   46,   46,
+       47,   47,   37,   47,   48,   48,   48,   49,   49,   32,
+       49,   50,   50,   31,   50,   51,   51,   51,   51,   52,
+       52,   28,   52,   53,   25,   12,   53,    3,   45,   45,
+       45,   45,   45,   45,   45,   45,   45,   45,   45,   45,
+       45,   45,   45,   45,   45,   45,   45,   45,   45,   45,
+       45
+
     } ;
 
 static yy_state_type yy_state_buf[YY_BUF_SIZE + 2], *yy_state_ptr;
@@ -408,7 +416,7 @@ char *yytext;
 #define INITIAL 0
 #line 4 "lexer1.l"
 #include "lexer1.h"
-#line 412 "lex.yy.c"
+#line 420 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -561,7 +569,7 @@ YY_DECL
 
 #line 7 "lexer1.l"
 
-#line 565 "lex.yy.c"
+#line 573 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -609,14 +617,14 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 41 )
+				if ( yy_current_state >= 46 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			*yy_state_ptr++ = yy_current_state;
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 50 );
+		while ( yy_base[yy_current_state] != 69 );
 
 yy_find_action:
 		yy_current_state = *--yy_state_ptr;
@@ -675,97 +683,97 @@ return -7;
 case 5:
 YY_RULE_SETUP
 #line 12 "lexer1.l"
-return PLUS;
+return -8;
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
 #line 13 "lexer1.l"
-return MINUS;
+return -9;
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
 #line 14 "lexer1.l"
-return MULT;
+return PLUS;
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
 #line 15 "lexer1.l"
-return DIVIDE;
+return MINUS;
 	YY_BREAK
 case 9:
 YY_RULE_SETUP
 #line 16 "lexer1.l"
-return MODULO;
+return MULT;
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
 #line 17 "lexer1.l"
-return EXP;
+return DIVIDE;
 	YY_BREAK
 case 11:
 YY_RULE_SETUP
 #line 18 "lexer1.l"
-return LPAREN;
+return MODULO;
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
 #line 19 "lexer1.l"
-return RPAREN;
+return EXP;
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
 #line 20 "lexer1.l"
-return EQUALS;
+return LPAREN;
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
 #line 21 "lexer1.l"
-return LTHAN;
+return RPAREN;
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
 #line 22 "lexer1.l"
-return GTHAN;
+return EQUALS;
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
 #line 23 "lexer1.l"
-return COLON;
+return LTHAN;
 	YY_BREAK
 case 17:
 YY_RULE_SETUP
 #line 24 "lexer1.l"
-return COMMA;
+return GTHAN;
 	YY_BREAK
 case 18:
 YY_RULE_SETUP
 #line 25 "lexer1.l"
-return SCOLON;
+return COLON;
 	YY_BREAK
 case 19:
 YY_RULE_SETUP
 #line 26 "lexer1.l"
-return PERIOD;
+return COMMA;
 	YY_BREAK
 case 20:
 YY_RULE_SETUP
 #line 27 "lexer1.l"
-return QUOTE;
+return SCOLON;
 	YY_BREAK
 case 21:
 YY_RULE_SETUP
 #line 28 "lexer1.l"
-return DQUOTE;
+return PERIOD;
 	YY_BREAK
 case 22:
 YY_RULE_SETUP
 #line 29 "lexer1.l"
-return NUMBER;
+return QUOTE;
 	YY_BREAK
 case 23:
 YY_RULE_SETUP
 #line 30 "lexer1.l"
-return NUMBER;
+return DQUOTE;
 	YY_BREAK
 case 24:
 YY_RULE_SETUP
@@ -779,15 +787,30 @@ return NUMBER;
 	YY_BREAK
 case 26:
 YY_RULE_SETUP
-#line 34 "lexer1.l"
-return -1;
+#line 33 "lexer1.l"
+return NUMBER;
 	YY_BREAK
 case 27:
 YY_RULE_SETUP
-#line 36 "lexer1.l"
+#line 34 "lexer1.l"
+return NUMBER;
+	YY_BREAK
+case 28:
+YY_RULE_SETUP
+#line 35 "lexer1.l"
+return NUMBER;
+	YY_BREAK
+case 29:
+YY_RULE_SETUP
+#line 38 "lexer1.l"
+return -1;
+	YY_BREAK
+case 30:
+YY_RULE_SETUP
+#line 40 "lexer1.l"
 ECHO;
 	YY_BREAK
-#line 791 "lex.yy.c"
+#line 814 "lex.yy.c"
 			case YY_STATE_EOF(INITIAL):
 				yyterminate();
 
@@ -1076,7 +1099,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 41 )
+			if ( yy_current_state >= 46 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -1106,11 +1129,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 41 )
+		if ( yy_current_state >= 46 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 40);
+	yy_is_jam = (yy_current_state == 45);
 	if ( ! yy_is_jam )
 		*yy_state_ptr++ = yy_current_state;
 
@@ -1671,7 +1694,7 @@ int main()
 	return 0;
 	}
 #endif
-#line 36 "lexer1.l"
+#line 40 "lexer1.l"
 
 
 
